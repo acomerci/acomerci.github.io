c7ec98b39d15b53554c39b8fd5a326f99f1044bd
Alexis Comerci <alexiscomerci@gmail.com>
2019-07-27 17:45:48 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -6,18 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://media.tenor.com/images/2e2f908309dd8a576d1193506c16f147/tenor.gif" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mir&aacute;, tengo una p&aacute;gina.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
