4e6a5c155d884d65a8311088f6b8550f9df613c4
Alexis Comerci <alexiscomerci@gmail.com>
2019-07-29 21:35:43 -0300

    Material UI.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,94 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import SwipeableViews from 'react-swipeable-views';
+
 import logo from './logo.svg';
 import './App.css';
+import Clock from './Clock.js';
 
 function App() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  function handleChangeIndex(index) {
+    setValue(index);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://media.tenor.com/images/2e2f908309dd8a576d1193506c16f147/tenor.gif" className="App-logo" alt="logo" />
-        <p>
-          Mir&aacute;, tengo una p&aacute;gina.
-        </p>
+        <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} centered>
+          <Tab label="Bienvenida" {...a11yProps(0)} />
+          <Tab label="Hora" {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          <img src="https://media.tenor.com/images/2e2f908309dd8a576d1193506c16f147/tenor.gif" className="App-logo" alt="logo" />
+          <p>
+            Mir&aacute;, tengo una p&aacute;gina.
+          </p>
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+        <Clock />
+        </TabPanel>
+      </SwipeableViews>
       </header>
     </div>
   );
 }
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 export default App;
