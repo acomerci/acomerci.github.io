259881350554cb7de3f1ab0d78189978856e2d7e
Alexis Comerci <alexiscomerci@gmail.com>
2019-08-04 11:17:22 -0300

    Todo en una página + grow.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import SwipeableViews from 'react-swipeable-views';
-import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 
 import './App.css';
 import TitlebarGridList from './Projects';
-import Clock from './Clock.js';
 
 function App() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
-
-  function handleChange(event, newValue) {
-    setValue(newValue);
-  }
-
-  function handleChangeIndex(index) {
-    setValue(index);
-  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <AppBar position="sticky">
+        {/* <AppBar position="sticky">
         <Tabs value={value} onChange={handleChange} centered>
-          <Tab label="Bienvenida" {...a11yProps(0)} />
+          <Tab label="Proyectos" {...a11yProps(0)} />
           <Tab label="Hora" {...a11yProps(1)} />
-          <Tab label="Proyectos" {...a11yProps(2)} />
+          <Tab label="Bienvenida" {...a11yProps(2)} />
         </Tabs>
       </AppBar>
       <SwipeableViews
@@ -42,59 +20,22 @@ function App() {
         index={value}
         onChangeIndex={handleChangeIndex}
       >
-        <TabPanel value={value} index={0} dir={theme.direction}>
+      <TabPanel value={value} index={0} dir={theme.direction}> */}
+        <TitlebarGridList />
+      {/* </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          <Clock />
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
           <img src="https://media.tenor.com/images/2e2f908309dd8a576d1193506c16f147/tenor.gif" className="App-logo" alt="logo" />
           <p>
             Mir&aacute;, tengo una p&aacute;gina.
           </p>
         </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
-          <Clock />
-        </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-          <TitlebarGridList />
-        </TabPanel>
-      </SwipeableViews>
+      </SwipeableViews> */}
       </header>
     </div>
   );
 }
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
 
 export default App;
--- src/Projects.js
+++ src/Projects.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Grow from '@material-ui/core/Grow';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import tileData from './tileData';
 
 const useStyles = makeStyles(theme => ({
@@ -16,6 +16,12 @@ const useStyles = makeStyles(theme => ({
         flexWrap: 'wrap',
         justifyContent: 'space-around',
         overflow: 'hidden',
+    },
+    welcome: {
+        height: 'calc(100vh + 53px)',
+    },
+    gridContainer: {
+        margin: '0',
     },
     titlePositionBottom: {
         bottom: '7px',
@@ -34,24 +40,96 @@ const useStyles = makeStyles(theme => ({
 export default function TitlebarGridList() {
     const classes = useStyles();
 
+    const [scroll, setScroll] = useState(0);
+    const [scrollItems, setScrollItems] = useState([]);
+
+    useEffect(() => {
+        function onScrollOrResize() {
+            setScroll((window.pageYOffset || document.documentElement.scrollTop) - (document.documentElement.clientTop || 0) +
+                getViewportHeight());
+        }
+
+        // onScrollOrResize();
+
+        window.addEventListener('scroll', onScrollOrResize);
+        window.addEventListener('resize', onScrollOrResize);
+
+        return () => {
+            window.removeEventListener('scroll', onScrollOrResize);
+            window.removeEventListener('resize', onScrollOrResize);
+        }
+    }, [])
+
+    useEffect(() => {
+        var elemScroll = [];
+        var elems = document.getElementsByName('scrollGrow');
+        for (var i = 0; i < elems.length; i++) {
+            var elem = elems[i];
+            var scrollItem = scrollItems.find(si => si.id == elem.id);
+            var grow = scrollItem ? scrollItem.grow || (scrollItem.scroll > getViewportHeight() && scrollItem.scroll < scroll) : false;
+            if (grow)
+                console.log(scrollItem.scroll + ' ' + scroll)
+            elemScroll.push({ id: elem.id, scroll: elem.offsetTop - elem.scrollTop + elem.clientTop, grow: grow });
+        }
+
+        setScrollItems(elemScroll);
+    }, [scroll]);
+
+    function getGrow(id) {
+        var item = scrollItems.find(si => si.id == id);
+        // console.log(item);
+
+        // if (item && !item.scroll) {
+        //     item = document.getElementById(id);
+        //     console.log(item);
+        //     item = { scroll: item.offsetTop - item.scrollTop + item.clientTop };
+        // }
+
+        // console.log(scroll + ': ' + (item ? item.scroll : 0));
+        return item && item.grow;
+    }
+
+    function getViewportHeight() {
+        return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    }
+
     return (
         <div className={classes.root}>
-            <Grid container spacing={3}>
-                {tileData.map(tile => (
+            <Box
+                display="flex"
+                alignItems="center"
+                css={{ height: 'calc(100vh + 53px)' }}
+                className={classes.welcome}>
+                <Typography variant="h1" component="h2">
+                    Hola
+                </Typography>
+            </Box>
+            <Grid
+                container
+                spacing={3}
+                className={classes.gridContainer}>
+                {tileData.map((tile, index) => (
                     <Grid key={tile.img} item sm={12} md={6}>
-                    <GridListTile className={classes.li}>
-                            <img src={tile.img} alt={tile.title} className={classes.img} />
-                            <GridListTileBar
-                                title={tile.title}
-                                subtitle={<span>{tile.client}</span>}
-                                className={classes.titlePositionBottom}
-                                actionIcon={
-                                    <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
-                                        <InfoIcon />
-                                    </IconButton>
-                                }
-                            />
-                        </GridListTile>
+                        <Grow
+                            in={getGrow('img_' + index)}
+                            timeout={1000}>
+                            <GridListTile
+                                className={classes.li}
+                                id={'img_' + index}
+                                name={'scrollGrow'}>
+                                <img src={tile.img} alt={tile.title} className={classes.img} />
+                                <GridListTileBar
+                                    title={tile.title}
+                                    subtitle={<span>{tile.client}</span>}
+                                    className={classes.titlePositionBottom}
+                                    actionIcon={
+                                        <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
+                                            <InfoIcon />
+                                        </IconButton>
+                                    }
+                                />
+                            </GridListTile>
+                        </Grow>
                     </Grid>
                 ))}
             </Grid>
