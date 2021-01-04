0610b0c79f18666c656f20578336e693a82ccaf6
Alexis Comerci <alexis.comerci@sistemasactivos.com>
2021-01-04 17:17:35 -0300

    Empiezo devuelta

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
-import React from 'react';
-
-import './App.css';
-import TitlebarGridList from './Projects';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
-        {/* <AppBar position="sticky">
-        <Tabs value={value} onChange={handleChange} centered>
-          <Tab label="Proyectos" {...a11yProps(0)} />
-          <Tab label="Hora" {...a11yProps(1)} />
-          <Tab label="Bienvenida" {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-      <TabPanel value={value} index={0} dir={theme.direction}> */}
-        <TitlebarGridList />
-      {/* </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
-          <Clock />
-        </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-          <img src="https://media.tenor.com/images/2e2f908309dd8a576d1193506c16f147/tenor.gif" className="App-logo" alt="logo" />
-          <p>
-            Mir&aacute;, tengo una p&aacute;gina.
-          </p>
-        </TabPanel>
-      </SwipeableViews> */}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
