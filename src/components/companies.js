60c2d5a79d4725fe6e3a1b76ac8fc18012aedf5d
Alexis Comerci <alexiscomerci@gmail.com>
2021-01-11 22:37:40 -0300

    Efecto empresas.

--- src/components/companies.js
+++ src/components/companies.js
@@ -1,27 +1,36 @@
-import { Box, Container, Grid } from "@material-ui/core";
-import React, { Component } from "react";
+import {
+  Box,
+  Container,
+  Grid,
+  Slide,
+  useScrollTrigger,
+} from "@material-ui/core";
+import React from "react";
 import Locale from "utils/localization";
 import companies from "../assets/data/companies.json";
 import Company from "./company";
 
-class Companies extends Component {
-  render() {
-    return (
-      <Box id="companies" pt="25px" pb="150px">
-        <Container>
-          <h1>{Locale.companies_title}</h1>
-          <p>{Locale.companies_description}</p>
-          <Box pt="20px">
-            <Grid container spacing={10}>
-              {companies.map((company, index) => (
-                <Company key={index} company={company} />
-              ))}
-            </Grid>
-          </Box>
-        </Container>
-      </Box>
-    );
-  }
+export default function Companies() {
+  const trigger = useScrollTrigger();
+  return (
+    // <Slide direction="right" in={trigger}>
+    <Box id="companies" pt="25px" pb="150px">
+      <Container>
+        <h1>{Locale.companies_title}</h1>
+        <p>{Locale.companies_description}</p>
+        <Box pt="20px">
+          <Grid container spacing={10}>
+            {companies.map((company, index) => (
+              <Company
+                key={index}
+                company={company}
+                delay={`${50 * index}ms`}
+              />
+            ))}
+          </Grid>
+        </Box>
+      </Container>
+    </Box>
+    // </Slide>
+  );
 }
-
-export default Companies;
--- src/components/company.js
+++ src/components/company.js
@@ -1,4 +1,10 @@
-import { Grid, makeStyles } from "@material-ui/core";
+import {
+  Grid,
+  makeStyles,
+  Slide,
+  useScrollTrigger,
+  Zoom,
+} from "@material-ui/core";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({
@@ -11,10 +17,17 @@ export default function Company(props) {
   const company = props.company;
   const imgSrc = require(`../assets/img/${company.img}`).default;
   const classes = useStyles();
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 800,
+  });
+  console.log(trigger);
 
   return (
-    <Grid item xs={12} sm={6} md={company.md}>
-      <img src={imgSrc} alt={company.alt} className={classes.img} />
-    </Grid>
+    <Zoom in={trigger} style={{ transitionDelay: props.delay }}>
+      <Grid item xs={6} sm={4} md={company.md}>
+        <img src={imgSrc} alt={company.alt} className={classes.img} />
+      </Grid>
+    </Zoom>
   );
 }
