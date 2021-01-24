7869fc3c6b8efb9d27ce8873d510bbf01230530e
Alexis Comerci <alexiscomerci@gmail.com>
2021-01-24 16:46:44 -0300

    Mostrar efecto de empresas una sola vez.

--- src/components/company.js
+++ src/components/company.js
@@ -11,13 +11,19 @@ export default function Company(props) {
   const company = props.company;
   const imgSrc = require(`../assets/img/companies/${company.img}`).default;
   const classes = useStyles();
+  const [transitionShown, setTransitionShown] = React.useState(false);
+
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 300,
   });
 
   return (
-    <Zoom in={trigger} style={{ transitionDelay: props.delay }}>
+    <Zoom
+      in={transitionShown || trigger}
+      style={{ transitionDelay: props.delay }}
+      onEnter={() => setTransitionShown(true)}
+    >
       <Grid item xs={6} sm={4} md={company.md}>
         <img src={imgSrc} alt={company.alt} className={classes.img} />
       </Grid>
